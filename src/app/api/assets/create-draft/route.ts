ba479cb1bc1ab3d59e22b17b4d3c11761c365ee0
jwbnw <jwbnw@icloud.com>
2026-03-26 22:33:21 -0700

    gets create-draft route working with Postman test. Writes to DB and Supabase buckets

--- src/app/api/assets/create-draft/route.ts
+++ src/app/api/assets/create-draft/route.ts
@@ -16,14 +16,29 @@ export async function POST(req: Request) {
     const formData = await req.formData();
     const file = formData.get('file') as File;
     const pixelHash = formData.get('pixelHash') as string;
+    const pHash = formData.get('pHash') as string;
     const ownerWallet = formData.get('ownerWallet') as string; // Temp: Trusting the frontend
+    const manifestHash = formData.get('manifestHash') as string;
+    const manifestJson = formData.get('manifestJson ') as string;
+    const licenseHash  = formData.get('licenseHash') as string;
+    const licenseType = formData.get('licenseType') as string;
+    const priceLamports = formData.get('priceLamports') as string;
 
-    const fileName = `${pixelHash}-${Date.now()}.jpg`;
+    // TODO: const trustLevel = formData.get('trustLevel') as string; // We will need to build a class to examine the manifest on the server to check if hardware or software 
+
     
+    const fileName = `${pixelHash}${Date.now()}.png`;
+    
+    // Probably do serverside verification of the file type and size here in a production environment to prevent abuse, 
+    //but for now we will trust the frontend.
+
     // Upload to Storage
     const { error: storageError } = await supabaseAdmin.storage
       .from('assets')
-      .upload(fileName, file);
+      .upload(fileName, file, {contentType: 'image/png', // Use the actual file type detected
+        cacheControl: '3600',
+        upsert: false})
+      ;
 
     if (storageError) throw storageError;
 
@@ -36,6 +51,12 @@ export async function POST(req: Request) {
       .from('assets')
       .insert({
         pixel_hash: pixelHash,
+        p_hash: pHash,
+        manifest_hash: manifestHash,
+        manifest_json: manifestJson,
+        license_hash: licenseHash,
+        license_type: licenseType,
+        price_lamports: priceLamports,
         owner_wallet: ownerWallet,
         image_url: publicUrl,
         status: 'draft',
@@ -43,10 +64,11 @@ export async function POST(req: Request) {
       .select()
       .single();
 
-    if (dbError) throw dbError;
+    if (dbError) throw dbError; // In a production environment, we would want to handle this more gracefully, potentially rolling back the storage upload if the database insert fails. Also Log..
 
     return NextResponse.json({ assetId: asset.id });
   } catch (err: any) {
+    // probs log this and not return to the client
     return NextResponse.json({ error: err.message }, { status: 500 });
   }
 }
